2b544b3bdd79b203b82d606c1ea67a8d587e8b9e
Kasey Le <kaseyle@stanford.edu>
2014-02-07 00:13:05 -0800

    Tweak routes

--- app.js
+++ app.js
@@ -42,7 +42,6 @@ app.get('/', index.view);
 app.get('/clock', clock.view);
 app.get('/cloud', cloud.view);
 app.get('/yoga', yoga.view);
-app.post('/yoga', yoga.view);
 // Example route
 // app.get('/users', user.list);
 
