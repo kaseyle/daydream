f3241e084a12a6c96aaa11346570043c991efffa
Marielos <msanson@stanford.edu>
2014-02-12 19:59:20 +0000

    had forgotten app.get

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var cloud = require('./routes/cloud');
 var clock = require('./routes/clock');
 var yoga = require('./routes/yoga');
 var cloudGazing = require('./routes/cloudGazing');
-var reflect_day = require('./routes/reflect_day')
+var reflect_day = require('./routes/reflect_day');
+var meditate = require('./routes/meditate');
 // Example route
 // var user = require('./routes/user');
 
@@ -46,6 +47,7 @@ app.get('/cloud', cloud.view);
 app.get('/yoga', yoga.view);
 app.get('/cloudGazing', cloudGazing.view);
 app.get('/reflect_day', reflect_day.view);
+app.get('/meditate', meditate.view);
 // Example route
 // app.get('/users', user.list);
 
