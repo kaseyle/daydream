db02dc1dc88a968fda24697601c7421002bef5d1
vagrant <vagrant@precise32.(none)>
2014-02-12 21:56:32 +0000

    modified clock for iphone

--- public/js/clock.js
+++ public/js/clock.js
@@ -5,46 +5,38 @@ $(document).ready(function() {
 	initializePage();
 })
 
-/*
- * Function that is called when the document is ready.
- */
-//function initializePage() {
-//	console.log("Javascript connected!");
-//	$("#dragbox").click(function(e) {
-//		console.log("You clicked the bar...");
-//		//here's what happens
-//		e.preventDefault();
-//		var mouseX = e.pageX;
-///		var mouseY = e.pageY;
-	//	$("this").color(black);
-//	}
-//}
 function initializePage() {
 	console.log("Javascript connected!");
 	$("#timechosen").hide();
 	var firstClick = true;
-	$(document).click(function(e) {
+	$("#container").click(function(e) {
 		console.log("You clicked the bar!");
-		//e.preventDefault();
+		var dwidth = $(document).width();
+		var cwidth = $("#container").width();
+		var buffer = (dwidth-cwidth)/2;
 		var mouseX = e.pageX;
 		var mouseY = e.pageY;
 
-		if (mouseY > 435 && mouseY < 530) {
-			if (mouseX < 428) $("#timechosen").text("0-2 minutes");
-			else if (mouseX < 550) $("#timechosen").text("2-5 minutes");
-			else if (mouseX < 680) $("#timechosen").text("5-10 minutes");
-			else if (mouseX < 810) $("#timechosen").text("10-15 minutes");
-			else if (mouseX < 945) $("#timechosen").text("15-30 minutes");
-			else if (mouseX < 1066) $("#timechosen").text("30-60 minutes");
-		}
+		var boxStart = 0.05*cwidth;
+		var boxEnd = 0.95*cwidth;
+		var boxWidth = .9*cwidth;
+		var mouseXAdj = mouseX-(buffer+boxStart);
+
+			if (mouseXAdj < 0.16*boxWidth) $("#timechosen").text("0-2 minutes");
+			else if (mouseXAdj < 0.32*boxWidth) $("#timechosen").text("2-5 minutes");
+			else if (mouseXAdj < 0.40*boxWidth) $("#timechosen").text("5-10 minutes");
+			else if (mouseXAdj < 0.70*boxWidth) $("#timechosen").text("10-15 minutes");
+			else if (mouseXAdj < 0.86*boxWidth) $("#timechosen").text("15-30 minutes");
+			else if (mouseXAdj < boxWidth) $("#timechosen").text("30-60 minutes");
 
 		if (firstClick) {
 			firstClick = false;
 			$("#timechosen").fadeIn();
 		}
-		if (mouseY > 435 && mouseY < 530 && mouseX < 1066) {
+
+		if (mouseXAdj > 0 && mouseXAdj < boxWidth) {
 			$("#dragbox").animate({
-				width: mouseX-270
+				width: mouseXAdj-3
 			})
 		}
 	});
