8a5ff6a998b53a22f97be0a1f4ec620d503f22cd
Marielos <msanson@stanford.edu>
2014-02-14 04:27:44 +0000

    Done with simple activities

--- public/js/positiveThinking.js
+++ public/js/positiveThinking.js
@@ -1,10 +1,12 @@
 var prompts = [
-	"Be kind. Getting into the habit of being kind and forgiving to others will help you extend the same courtesy to yourself",
-	"Think of one thing you are excited for today.",
-	"Think of one thing you are grateful for today.",
-	"Think of one thing that made you laugh today.",
-	"Think of one thing you are proud of today.",
-	"Think of one person you want to be happy today."
+	"Turn your CAN'TS into CANS: From 'why do I always get bad grades' to 'Next time I will do better'.",
+	"Be kind. Getting into the habit of being kind and forgiving to others will help you extend the same courtesy to yourself.",
+	"Remember that events and situations aren't inherently good or bad -- we just project those descriptions onto them.",
+	"Become the captain of your own ship. Instead of worrying what other people think of you and trying to live up the their standards, decide for yourself who you want to be and what you want to accomplish.",
+	"Never forger to laugh. Laughter relieves stress and reminds you not to take life so seriously.",
+	"Remind yourself daily that although we may not be able to control what happens to us, we are always in charge of our reactions.",
+	"Having people around who see the best in you will help you see the best in yourself.",
+	"Remembering all the good things that you have makes it a whole lot easier not to focus on what you don't have."
 ]
 
 var index = 1;
