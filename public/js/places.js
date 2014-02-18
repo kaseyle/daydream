2f96ca43ab8a8213cc592f2857fddd76a0287b20
vagrant <vagrant@precise32.(none)>
2014-02-18 05:49:33 +0000

    Change places and quotes to local images

--- public/js/places.js
+++ public/js/places.js
@@ -1,17 +1,16 @@
 var urls = [
-	"http://media-cache-ak0.pinimg.com/736x/94/b0/4d/94b04d18777846902bccf184197ef0ec.jpg",
-	"http://media-cache-ec0.pinimg.com/736x/bd/ac/b6/bdacb627207b209d6fd0efe825b8dc6d.jpg",
-	"http://media-cache-ak0.pinimg.com/736x/46/88/15/468815b902435b03789b9180f4036869.jpg",
-	"http://media-cache-ec0.pinimg.com/736x/15/68/b4/1568b4e1a55070f81cc447e442f29b83.jpg",
-	"http://media-cache-ak0.pinimg.com/736x/5d/92/ca/5d92ca2dac1aabb9c44cf93a9a34bd65.jpg",
-	"http://media-cache-ak0.pinimg.com/736x/1c/7f/4e/1c7f4ed295e1396290f7697e7fd54ba3.jpg",
-	"http://media-cache-ec0.pinimg.com/736x/87/a3/d9/87a3d9fdbd84dc091917893e20131b6f.jpg",
-	"http://media-cache-ec0.pinimg.com/736x/93/49/44/9349442868e770563090eb1a6f8b9eaa.jpg",
-	"http://media-cache-ec0.pinimg.com/736x/24/91/e1/2491e13fd9c834ed0649662c83451e60.jpg",
-	"http://media-cache-ec0.pinimg.com/736x/4f/91/50/4f9150bd5844cabdb035a8037527ed4d.jpg",
-	"http://media-cache-ec0.pinimg.com/736x/87/d3/34/87d33439ec6eb1d0812fb8a84e387d83.jpg",
-	"http://media-cache-ak0.pinimg.com/736x/97/98/23/97982345d8c49e0d2fda503782abfd10.jpg"
-
+	"http://daydreaming.herokuapp.com/images/places_1.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_2.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_3.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_4.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_5.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_6.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_7.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_8.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_9.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_10.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_11.jpeg",
+	"http://daydreaming.herokuapp.com/images/places_12.jpeg"
 ]
 
 var names = [
