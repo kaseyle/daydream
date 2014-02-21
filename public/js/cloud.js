0c9c5abfd27d5ac916c67f821fa8589182d7d2e0
vagrant <vagrant@precise32.(none)>
2014-02-21 07:34:42 +0000

    Add activities to data

--- public/js/cloud.js
+++ public/js/cloud.js
@@ -64,8 +64,8 @@ function outListner(event, ui) {
 }
 
 function getActivities() {
-	//var temp = data["times"][time_range];
-	var temp = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
+	var temp = data["times"][time_range];
+	//var temp = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
 	var activities = temp.slice(0);
 	for (var i = 0; i < words.length; i++) {
 		temp = data["tags"][words[i].toLowerCase()];
