e62189bdaabe051e365cfac05d56667861800647
vagrant <vagrant@precise32.(none)>
2014-03-07 08:39:15 +0000

    fixed clock page

--- public/js/clock.js
+++ public/js/clock.js
@@ -9,79 +9,40 @@ var time_range = 2;
 
 function initializePage() {
 	console.log("Javascript connected!");
-	$("#next").click(clickListener);
-	$("#timechosen").hide();
 	var firstClick = true;
+	$("#timechosen").hide();
+
+	$( ".draggable" ).draggable({containment: "parent"});
+	$( "#slider").slider({
+		range: "min", 
+		value: 7,
+		min: 0,
+		max: 60,
+		slide: function(event, ui) {
+			if (firstClick) {
+				firstClick = false;
+				$("#timechosen").fadeIn();
+			}
+			var timevalue = $("#slider").slider("value");
+			$("#time").text(timevalue + " minutes");
+			if(timevalue<3) {
+				time_range = 2;
+			}else if (timevalue < 6) {
+				time_range = 5;
+			}else if (timevalue < 11) {
+				time_range = 10;
+			}else if (timevalue < 16) {
+				time_range = 15;
+			}else if (timevalue < 31) {
+				time_range = 30;
+			}else time_range = 60;
+		}
+	});	
+
+
+	$("#next").click(clickListener);
 	$(".box").click(function(e) {
 		console.log("You clicked the bar!");
-		var dwidth = $(document).width();
-		var cwidth = $("#container").width();
-		var buffer = (dwidth-cwidth)/2;
-		var mouseX = e.pageX;
-		var mouseY = e.pageY;
-
-		var boxStart = 0.05*cwidth;
-		var boxEnd = 0.95*cwidth;
-		var boxWidth = .9*cwidth;
-		var mouseXAdj = mouseX-(buffer+boxStart);
-
-			if (mouseXAdj < 0.08*boxWidth) {
-				time_range = 2;
-				$("#timechosen").text("1 minute");
-			} else if (mouseXAdj < 0.16*boxWidth) {
-				time_range = 2;
-				$("#timechosen").text("2 minutes");
-			} else if (mouseXAdj < 0.24*boxWidth) {
-				time_range = 5;
-				$("#timechosen").text("3 minutes");
-			} else if (mouseXAdj < 0.28*boxWidth) {
-				time_range = 5;
-				$("#timechosen").text("4 minutes");
-			} else if (mouseXAdj < 0.36*boxWidth) {
-				time_range = 5;
-				$("#timechosen").text("5 minutes"); 
-			} else if (mouseXAdj < 0.40*boxWidth) {
-				time_range = 10;
-			 	$("#timechosen").text("7 minutes");
-			} else if (mouseXAdj < 0.50*boxWidth) {
-				time_range = 10;
-				$("#timechosen").text("10 minutes");
-			} else if (mouseXAdj < 0.58*boxWidth) {
-				time_range = 15;
-				$("#timechosen").text("13 minutes");
-			} else if (mouseXAdj < 0.68 *boxWidth) {
-				time_range = 15;
-				$("#timechosen").text("15 minutes");
-			} else if (mouseXAdj < 0.73*boxWidth) {
-				time_range = 30;
-				$("#timechosen").text("20 minutes");
-			} else if (mouseXAdj < 0.78*boxWidth) {
-				time_range = 30;
-				$("#timechosen").text("25 minutes");
-			} else if (mouseXAdj < 0.85*boxWidth) {
-				time_range = 30;
-				$("#timechosen").text("30 minutes");
-			} else if (mouseXAdj < 0.90*boxWidth) {
-				time_range = 60;
-				$("#timechosen").text("40 minutes");
-			} else if (mouseXAdj < 0.95*boxWidth) {
-				time_range = 60;
-				$("#timechosen").text("50 minutes");
-			} else if (mouseXAdj < boxWidth) {
-				time_range = 60;
-				$("#timechosen").text("60 minutes");
-			}
-
-		if (firstClick) {
-			firstClick = false;
-			$("#timechosen").fadeIn();
-		}
-
-		if (mouseXAdj > 0 && mouseXAdj < boxWidth) {
-			$("#dragbox").animate({
-				width: mouseXAdj-3
-			})
-		}
 	});
 }
 
