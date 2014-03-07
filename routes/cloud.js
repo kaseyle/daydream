4e85359319295c1e5b15d31598ed512246f7c65a
vagrant <vagrant@precise32.(none)>
2014-03-07 22:19:21 +0000

    Remove experiment code

--- routes/cloud.js
+++ routes/cloud.js
@@ -1,9 +1,11 @@
 exports.view = function (req, res) {
 	var time_range = req.query.time_range;
-	res.render('cloud', {"time_range": time_range, "alt": false});
+	res.render('cloud', {"time_range": time_range, "alt": true});
 }
 
+/*
 exports.viewAlt = function (req, res) {
 	var time_range = req.query.time_range;
 	res.render('cloud', {"time_range": time_range, "alt": true});
 }
+*/
